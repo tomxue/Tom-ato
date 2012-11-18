1c7f7ca74c9f7381b2bf678976bd19cbf8d1a234
Tom Xue <tomxue0126@gmail.com>
2012-11-18 23:17:15 +0800

    progressbar added; but this version causes phone dead

--- src/com/example/tompomodoros/MainActivity.java
+++ src/com/example/tompomodoros/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.tompomodoros;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 
 import java.util.Timer;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
+	private ProgressBar progressbar;
+	protected static final int STOP = 0x10000;
+	protected static final int NEXT = 0x10001;
+	private int iCount = 0;
+	private int TotalLength = 60 * 25;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -17,7 +25,10 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 
 		Button button_start = (Button) findViewById(R.id.button1);
+		ProgressBar progressbar = (ProgressBar) findViewById(R.id.progressBar1);
+
 		button_start.setOnClickListener(start);
+		progressbar.setIndeterminate(false);
 	}
 
 	@Override
@@ -32,15 +43,64 @@ public class MainActivity extends Activity {
 		public void onClick(View v) {
 			// 开启Service
 			// startService(new Intent("com.yarin.Android.MUSIC"));
-			Timer timer = new Timer();
-			timer.schedule(new MyTask(), 0, 1000);
+			// Timer timer = new Timer();
+			// timer.schedule(new MyTask(), 0, 1000);
+
+			// 创建一个线程,每秒步长为1增加,到100%时停止
+			progressbar.setVisibility(View.VISIBLE);
+			progressbar.setMax(TotalLength);
+			progressbar.setProgress(0);
+
+			Thread mThread = new Thread(new Runnable() {
+				public void run() {
+					for (iCount = 0; iCount < TotalLength; iCount++) {
+						try {
+							Thread.sleep(1000);
+							if (iCount == TotalLength) {
+								Message msg = new Message();
+								msg.what = STOP;
+								mHandler.sendMessage(msg);
+								break;
+							} else {
+								Message msg = new Message();
+								msg.what = NEXT;
+								mHandler.sendMessage(msg);
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+
+				}
+			});
+			mThread.start();
 		}
 	};
 
-	static class MyTask extends java.util.TimerTask {
-		@Override
-		public void run() {
-			System.out.println("________");
+	// static class MyTask extends java.util.TimerTask {
+	// @Override
+	// public void run() {
+	// System.out.println("________");
+	//
+	// }
+	// }
+
+	// 定义一个Handler
+	private Handler mHandler = new Handler() {
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case STOP:
+				progressbar.setVisibility(View.GONE);
+				Thread.currentThread().interrupt();
+				break;
+			case NEXT:
+				if (!Thread.currentThread().isInterrupted()) {
+					progressbar.setProgress(iCount);
+				}
+				break;
+			default:
+				break;
+			}
 		}
-	}
+	};
 }
