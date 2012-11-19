d95036abc282309ab6e5c6d057f9b3974760eb63
Tom Xue <tomxue0126@gmail.com>
2012-11-19 15:31:48 +0800

    change to use timer

--- src/com/example/tompomodoros/TomPomodoros.java
+++ src/com/example/tompomodoros/TomPomodoros.java
@@ -1,8 +1,8 @@
 package com.example.tompomodoros;
 
+import java.util.Timer;
+
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
@@ -11,13 +11,15 @@ import android.widget.ProgressBar;
 
 public class TomPomodoros extends Activity {
 	private ProgressBar progressbar;
-	private Button button_start;
+	private Button button_start, button_cancel;
 
 	protected static final int STOP = 0x10000;
 	protected static final int NEXT = 0x10001;
 
 	private int iCount = 0;
-	private int TotalLength = 1500;  // 60*25=1500
+	private int TotalLength = 1500; // 60*25=1500
+
+	Timer timer_tmp;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -25,41 +27,38 @@ public class TomPomodoros extends Activity {
 		setContentView(R.layout.activity_main);
 
 		button_start = (Button) findViewById(R.id.button1);
+		button_cancel = (Button) findViewById(R.id.button2);
 		progressbar = (ProgressBar) findViewById(R.id.progressBar1);
 		progressbar.setIndeterminate(false);
-		
+
 		button_start.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
 				// 创建一个线程,每秒步长为1增加,到100%时停止
 				progressbar.setVisibility(View.VISIBLE);
 				progressbar.setMax(TotalLength);
 				progressbar.setProgress(0);
-
-				Thread mThread = new Thread(new Runnable() {
-					public void run() {
-						for (iCount = 0; iCount < TotalLength; iCount++) {
-							try {
-								Thread.sleep(1000);
-								if (iCount == TotalLength) {
-									Message msg = new Message();
-									msg.what = STOP;
-									mHandler.sendMessage(msg);
-									break;
-								} else {
-									Message msg = new Message();
-									msg.what = NEXT;
-									mHandler.sendMessage(msg);
-								}
-							} catch (Exception e) {
-								e.printStackTrace();
-							}
-						}
-
-					}
-				});
-				mThread.start();
+				
+				Timer timer = new Timer();
+				timer.schedule(new ProgressbarTask(), 0, 1000);
+				timer_tmp = timer;
 			}
 		});
+
+		button_cancel.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				iCount = 0;
+				progressbar.setProgress(0);
+				timer_tmp.cancel();
+			}
+		});
+	}
+
+	private class ProgressbarTask extends java.util.TimerTask {
+		@Override
+		public void run() {
+			iCount += 1;
+			progressbar.setProgress(iCount);
+		}
 	}
 
 	@Override
@@ -67,22 +66,4 @@ public class TomPomodoros extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-
-	private Handler mHandler = new Handler() {
-		public void handleMessage(Message msg) {
-			switch (msg.what) {
-			case STOP:
-				progressbar.setVisibility(View.GONE);
-				Thread.currentThread().interrupt();
-				break;
-			case NEXT:
-				if (!Thread.currentThread().isInterrupted()) {
-					progressbar.setProgress(iCount);
-				}
-				break;
-			default:
-				break;
-			}
-		}
-	};
 }
