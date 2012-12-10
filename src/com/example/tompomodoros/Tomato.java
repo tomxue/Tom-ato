ae54a182a6b33c259fc777a60810f883185b161a
Tom Xue <tomxue0126@gmail.com>
2012-12-10 11:13:41 +0800

    try to add 'App Starts on Boot' function but it will cause phone dead

--- src/com/example/tompomodoros/Tomato.java
+++ src/com/example/tompomodoros/Tomato.java
@@ -33,7 +33,7 @@ public class Tomato extends Activity {
 	protected static final int NEXT = 0x10001;
 
 	private int iCount = 0;
-	private int TotalLength = 10; // 60*25=1500£¨25∑÷÷”/∑¨«—
+	private int TotalLength = 3; // 60*25=1500£¨25∑÷÷”/∑¨«—
 	private static String mydate_key;
 	private final String DBNAME = "tompomo12.db";
 	private static SQLiteDatabase db;
